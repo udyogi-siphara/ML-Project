c97d4809a1644bd411bbe209f4fe640dee3d2c1f
udyogi-siphara <udyogisiphara22@gmail.com>
2024-02-05 21:22:03 +0530

    Updated

---
+++ frontend/src/pages/api/data.js
@@ -0,0 +1,6 @@
+// pages/api/_data.js
+export const defaultRecommendations = [
+    { category: 'Groceries', item: 'Lunu', cost: 500 },
+    { category: 'Shopping', item: 'Watch', cost: 1500 },
+  ];
+  
